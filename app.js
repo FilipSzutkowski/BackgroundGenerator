29630c0e47db1b40f576cc9c58c87229d3e2fab8
Filip Szutkowski <63004141+FilipSzutkowski@users.noreply.github.com>
2020-07-01 03:11:01 +0200

    Add files via upload
    Added gradient degree manipulation and randomize button.

--- app.js
+++ app.js
@@ -3,35 +3,49 @@ const colorPickerOne = document.querySelector(`.colorOne`);
 const colorPickerTwo = document.querySelector(`.colorTwo`);
 const css = document.querySelector(`.css`);
 const h1 = document.querySelector(`h1`);
-let defaultColorOne = `#00240b`
-let defaultColorTwo = `#587901`
+const degree = document.querySelector(`.degree`);
+const typeLinear = document.querySelector(`#linear`);
+const typeRadial = document.querySelector(`#radial`);
+const newColors = document.querySelector(`.newColors`);
+
+let defaultDegree = `90`;
 
 window.addEventListener(`load`, startUp);
+newColors.addEventListener(`click`, startUp);
 
 function startUp() {
-    colorPickerOne.value = defaultColorOne;
-    colorPickerTwo.value = defaultColorTwo;
+    colorPickerOne.value = getRandomColor();
+    colorPickerTwo.value = getRandomColor();
+    degree.value = defaultDegree;
 
     changeBackgroundColor();
-    colorPickerOne.addEventListener(`input`, changeBackgroundColor)
-    colorPickerTwo.addEventListener(`input`, changeBackgroundColor)
+    colorPickerOne.addEventListener(`input`, changeBackgroundColor);
+    colorPickerTwo.addEventListener(`input`, changeBackgroundColor);
+    degree.addEventListener(`input`, changeDegree);
 }
 
 function changeBackgroundColor () {
-
     body.style.background =
-    "linear-gradient(to right, " 
+    `linear-gradient(` + degree.value + `deg, ` 
     + colorPickerOne.value 
-    + ", " 
+    + `, ` 
     + colorPickerTwo.value 
-    + ")";
+    + `)` + `no-repeat center center fixed`;
     h1.style.color = getRandomColor();
-    css.textContent = body.style.background + ";";
+}
+
+function changeDegree () {
+    body.style.background =
+    `linear-gradient(` + degree.value + `deg, ` 
+    + colorPickerOne.value 
+    + `, ` 
+    + colorPickerTwo.value 
+    + `)` + `no-repeat center center fixed`;    
 }
 
 function getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
+    let letters = `0123456789ABCDEF`;
+    let color = `#`;
     for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
